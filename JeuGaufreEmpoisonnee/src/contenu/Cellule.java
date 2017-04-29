c9a503ea54aa693aaf510edd4c708ce79b6c76f7
jo_ch <jo_ch@192.168.1.15>
2017-04-29 16:52:21 +0200

    commit2

--- JeuGaufreEmpoisonnee/src/contenu/Cellule.java
+++ JeuGaufreEmpoisonnee/src/contenu/Cellule.java
@@ -1,5 +1,14 @@
 package contenu;
 
 public class Cellule {
-
+	
+	int valeur;
+	
+	public Cellule(int numLigne, int numColonne){
+		this.valeur = (int) Math.pow(3, numLigne) * (int) Math.pow(2, numColonne);
+	}
+	
+	public int getValeur(){
+		return this.valeur;
+	}
 }
